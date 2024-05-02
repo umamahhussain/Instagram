21af051117f4c8cb39571318c4654cb4c3060bc2
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2024-05-02 17:32:29 +0500

    Updated Gradient and Padding Signup.js

--- Front-end/src/pages/Signup.js
+++ Front-end/src/pages/Signup.js
@@ -1,69 +1,68 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { GiButterfly } from "react-icons/gi";
 import M from 'materialize-css'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
-    const navigate=useNavigate();
-    const [username,setUsername]=useState("")
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-    const PostData=()=>
-    {
-        fetch("/signup",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"  
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const PostData = () => {
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 username,
                 password,
-                email     
-            })      
-        }).then(res=>res.json())
-        .then(data=>
-            {
-                if(data.error)
-                {
-                    M.toast({html:data.error,classes:"rounded #ec407a pink lighten-1"})
+                email
+            })
+        }).then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    M.toast({ html: data.error, classes: "rounded #ec407a pink lighten-1" })
                 }
-                else
-                {
-                    M.toast({html:data.message,classes:"rounded #ec407a pink lighten-1"})
+                else {
+                    M.toast({ html: data.message, classes: "rounded #ec407a pink lighten-1" })
                     navigate('/login');
                 }
             })
-            .catch(err=>
-                {
-                    console.log(err)
-                });
+            .catch(err => {
+                console.log(err)
+            });
     }
 
     return (
         <div className="signup-card">
-            <div className="card">
+            <div className="card2">
                 <div className="authcard">
-                    <h2 style={{ fontFamily: "Grand Hotel, cursive", color: "rgb(255, 37, 146)", display: 'inline' }}>Instagram </h2>
-                    <div style={{ color: 'rgb(255, 37, 146)', display: 'inline-block' }}>
-                        <GiButterfly size="50px" />
+
+                    <h2 style={{ fontFamily: "Grand Hotel, cursive", color: "midnightblue", display: 'inline' }}>Instagram </h2>
+                    <div style={{ color: 'midnightblue', display: 'inline-block' }}>
+                        <GiButterfly size="40px" />
                     </div>
-                    <input type="text" placeholder="email"
-                    value={email} onChange={e=> setEmail(e.target.value)}/>
 
-                   
-                    <input type="text" placeholder="username"
-                    value={username} onChange={e=>setUsername(e.target.value)}
-                    />
-
-                
-                    <input type="password" placeholder="password"
-                    value={password} onChange={e=>setPassword(e.target.value)}/>
+                    <div className="inputfields" style={{ marginTop: "30px" }}>
+                       
+                        <input type="text" placeholder="email"
+                            value={email} onChange={e => setEmail(e.target.value)} style={{color:'midnightblue'}}/>
 
 
-                    <button className="btn" style={{ backgroundColor: "rgb(255, 37, 146)", color: "white" }} 
-                    type="submit" name="action" onClick={()=>PostData()}>Sign up</button>
+                        <input type="text" placeholder="username"
+                            value={username} onChange={e => setUsername(e.target.value)} style={{color:'midnightblue'}}
+                        />
+
+
+                        <input type="password" placeholder="password"
+                            value={password} onChange={e => setPassword(e.target.value)} style={{color:'midnightblue'}} />
+
+
+                    </div>
+                    <button className="btn" style={{ backgroundColor: "midnightblue", color: "white", marginTop: "35px" }}
+                        type="submit" name="action" onClick={() => PostData()}>Sign up</button>
                 </div>
-
             </div>
         </div>
     )
