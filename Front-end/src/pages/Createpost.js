ad6e78746c847c3caf94a2c95f1888d90db68451
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2024-05-02 23:25:37 +0500

    Updated Gradient and Centered Input Box Createpost.js

--- Front-end/src/pages/Createpost.js
+++ Front-end/src/pages/Createpost.js
@@ -61,36 +61,43 @@ const CreatePost = () => {
       });
   };
 
+
   return (
-    <div className="card input-filed" style={{ margin: '10px auto', maxWidth: '500px', padding: '30px', textAlign: 'center' }}>
-      <input
-        type="text"
-        placeholder="Caption"
-        value={caption}
-        onChange={(e) => setCaption(e.target.value)}
-      />
-      <form action="#">
-        <div className="file-field input-field">
-          <div className="btn" style={{ backgroundColor: 'rgb(255, 37, 146)' }}>
-            <span>File</span>
-            <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+    <div className='createpost' style={{
+      background: ' rgb(189, 195, 199)',
+      background: 'radial-gradient(circle, rgba(189, 195, 199, 1) 0%, rgba(44, 62, 80, 1) 100%)', height: '89vh', display: 'flex', justifyContent: 'center', alignItems: 'center'
+    }}>
+      <div className="card input-filed" style={{ margin: '10px auto', width: '500px', textAlign: 'center', padding: '30px' }}>
+        <input
+          type="text"
+          placeholder="Caption"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+        />
+        <form action="#">
+          <div className="file-field input-field">
+            <div className="btn" style={{ backgroundColor: 'midnightblue' }}>
+              <span>File</span>
+              <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+            </div>
+            <div className="file-path-wrapper">
+              <input className="file-path validate" type="text" />
+            </div>
           </div>
-          <div className="file-path-wrapper">
-            <input className="file-path validate" type="text" />
-          </div>
-        </div>
-        <button
-          className="btn waves-effect"
-          style={{ backgroundColor: 'rgb(255, 37, 146)', color: 'white' }}
-          type="button" // Change the type to button
-          name="action"
-          onClick={() => postImage()}
-        >
-          Upload
-        </button>
-      </form>
+          <button
+            className="btn waves-effect"
+            style={{ backgroundColor: 'midnightblue', color: 'white' }}
+            type="button" // Change the type to button
+            name="action"
+            onClick={() => postImage()}
+          >
+            Upload
+          </button>
+        </form>
+      </div>
     </div>
   );
+
 };
 
 export default CreatePost;
